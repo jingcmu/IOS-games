7db74e7214f7dbfb9f473662f61aeba6cb965f46
jingcmu <jingcmu@qq.com>
2014-06-28 16:36:38 -0700

    CrazyBullet init

---
+++ CrazyBullet.spritebuilder/Source/Plane.h
@@ -0,0 +1,13 @@
+//
+//  Plane.h
+//  CrazyBullet
+//
+//  Created by Jing on 6/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Plane : CCSprite
+
+@end
---
+++ CrazyBullet.spritebuilder/Source/Plane.m
@@ -0,0 +1,24 @@
+//
+//  Plane.m
+//  CrazyBullet
+//
+//  Created by Jing on 6/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Plane.h"
+
+@implementation Plane
+
+- (id)init {
+    self = [super init];
+    
+    if(self) {
+        CCLOG(@"Plane created");
+    }
+    
+    return self;
+}
+
+
+@end
